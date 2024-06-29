c8a973dd6bca44f07fde5608e655aaf753ba9ed0
Santinop145 <143087754+Santinop145@users.noreply.github.com>
2024-06-28 21:29:12 -0300

    Añade los archivos principales

---
+++ main.js
@@ -0,0 +1,17 @@
+let i = 1;
+function handleSubmit(event){
+    event.preventDefault();
+    let respuesta = document.createElement('h2');
+    if (document.querySelector('h2'))
+        document.querySelector('h2').remove();
+    if (i==1){
+        respuesta.textContent = 'Apretaste el boton' + ' ' + i + ' ' + 'vez';
+    }
+    else{
+        respuesta.textContent = 'Apretaste el boton' + ' ' + i + ' ' + 'veces';
+    }
+    event.target.append(respuesta);
+    document.getElementById('button').play();
+    event.target.reset();
+    i++;
+}
