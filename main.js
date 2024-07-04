0cf80016235f6a9f8123ba65b0352009114c7585
Santinop145 <143087754+Santinop145@users.noreply.github.com>
2024-07-04 00:40:49 -0300

    Borra la mecanica de desbloquear el autoclicker y hace que sea requerido comprarlo.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let i = 0;
-let j = 1;
+let j = 0;
 let clicks = document.createElement('h2');
 let div = document.getElementById('buttonpanel');
 div.appendChild(clicks);
@@ -7,9 +7,6 @@ div.appendChild(clicks);
 document.getElementById('boton').onclick = function buttonClicked(){
     event.preventDefault();
     document.getElementById('button').play();
-    if (i == 250){
-        achievement();
-    }
     i++;
 }
 
@@ -18,24 +15,22 @@ setInterval(updateClicks, 10);
 function updateClicks(){
     clicks.textContent = 'Clicks' + ' ' + '=' + ' ' + i;
 }
-function achievement(){
-    let logro = document.createElement('h3');
-    logro.textContent = '¡Clickeaste 250 veces! (Desbloqueaste el autoclicker)';
-    document.body.appendChild(logro);
-    document.getElementById('celebration').play();
-    let autoclicker = setInterval(autoclick, 1000);
-}
 
 function autoclick(){
     i += j;
 }
 
 function upgradeAutoclick(){
-    if (i >= 500 && i > 300 || j > 1 && i > 300){
-        i -= 300
+    if (j < 1){
+        let autoclicker = setInterval(autoclick, 1000);
+        j++;
+    }
+    else if (i >= 201){
+        i -= 200
         j += 1
+        document.getElementById('celebration').play();
     }
     else{
-        alert("No es posible mejorar ahora mismo, desbloquea el autoclicker a los 250 clicks y revisa que tengas mas de 100 clicks disponibles.")
+        alert("No tienes clicks disponibles")
     }
 }
