b6b584997059dbda4fa189ed96b48e6ba98defdb
Santinop145 <143087754+Santinop145@users.noreply.github.com>
2024-07-04 02:56:28 -0300

    actualiza javascript para añadir muchas mas funciones

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let i = 0;
 let j = 0;
-let precio = 200;
+let k = 0;
+let l = 1;
+let vipx2comprado = 0;
+let preciomanual = 100;
+let preciomejora = 200;
+let precioclicker = 500;
 let clicks = document.createElement('h2');
 let div = document.getElementById('buttonpanel');
 div.appendChild(clicks);
@@ -8,7 +13,7 @@ div.appendChild(clicks);
 document.getElementById('boton').onclick = function buttonClicked(){
     event.preventDefault();
     document.getElementById('button').play();
-    i++;
+    i += l;
 }
 
 setInterval(updateClicks, 10);
@@ -18,24 +23,67 @@ function updateClicks(){
 }
 
 function autoclick(){
-    i += j;
+    i += j * k;
+}
+
+function buyManualClick(){
+    if(i >= preciomanual){
+    l++;
+    document.getElementById('celebration').play();
+    i -= preciomanual;
+    preciomanual += 50;
+    }
+    else{
+        alert("No tienes suficientes clicks");
+    }
+}
+function buyAutoclick(){
+    if(k == 0 && i >= precioclicker){
+        autoclicker = setInterval(autoclick, 1000);
+        document.getElementById('celebration').play();
+        k++; 
+        j++;
+        i -= precioclicker;
+        precioclicker += 250;
+    }
+    else if(k < 10 && i >= precioclicker){
+        document.getElementById('celebration').play();
+        i -= precioclicker;
+        k++;
+        precioclicker += 250;
+    }
+    else if(k == 10){
+        alert("Has alcanzado el limite de autoclickers");
+    }
+    else{
+        alert("No tienes suficientes clicks");
+    }
 }
 
 function upgradeAutoclick(){
-    if (j < 1 && i >= precio){
-        let autoclicker = setInterval(autoclick, 1000);
+    if (i >= preciomejora){
         document.getElementById('celebration').play();
-        i -= precio;
+        i -= preciomejora;
         j++;
-        precio += 50;
-    }
-    else if (i >= precio){
-        document.getElementById('celebration').play();
-        i -= precio;
-        j++;
-        precio += 50;
+        preciomejora += 50;
     }
     else{
-        alert("No tienes clicks disponibles");
+        alert("No tienes suficientes clicks");
+    }
+}
+
+function buyVipX2(){
+    if (i >= 10000 && vipx2comprado == 0){
+        clearInterval(autoclicker);
+        setInterval(autoclick, 500);
+        i -= 10000;
+        vipx2comprado = 1;
+        document.getElementById('vipbuy').play();
+    }
+    else if (vipx2comprado => 1){
+        alert('¡Ya has comprado esta mejora!')
+    }
+    else{
+        alert("No tienes suficientes clicks");
     }
 }
